4e3f5e3d3585380b6f60a35a23604e417857850f
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-02-15 15:47:59 -0600

    finished

--- StudentClass.py
+++ StudentClass.py
@@ -1,38 +1,37 @@
-
+from datetime import date
 
 class Student:
 
-    def __init__(self, studID, name, date_of_birth, classification, register):
-        self.__StudentID = studID
-        self.__Name = name 
-        self.__dob = date_of_birth
-        self.__Classification = classification
-        self.__register = None
-        self.__stu_age = 0
+    def __init__(self, id, name, dob, classification):
+        self.__student_id = id
+        self.__name = name 
+        self.__dob = dob
+        self.__classification = classification
+        self.__register = ' '
+        self.__age = 0
 
-    def get_age(self, dob, stu_age):
-        self.__dob = '10/11/2001'
-        self.__dob = dob.split('/')
-        self.__dob_year = dob[2]
-        self.__stu_age = year - dob_year
-
-
-    def classification(self, Classification, register):
-        self.Classification = [F, S, Jr, Sr]
-        if self.Classification == F:
-            self.register = '4/10 thru 4/12'
-        if self.Classification == S:
-            self.register = '4/7 thru 4/9'
-        if self.Classification == Jr:
-            self.register = '4/4 thru 4/6'
-        if self.Classification == Sr:
+    def calc_age(self):
+        today = date.today()
+        dob = self.__dob.split('/')
+        dob_year = int(dob[2])
+        self.__age = today.year - dob_year
+        
+    def calc_register(self):
+        if self.__classification == 'senior':
             self.register = '4/1 thru 4/3'
+        elif self.__classification == 'junior':
+            self.register = '4/4 thru 4/6'
+        elif self.__classification == 'sophomore':
+            self.register = '4/7 thru 4/9'
+        elif self.__classification == 'freshman':
+            self.register = '4/10 thru 4/12'
+        else:
+            self.__register = 'classification not found'
 
+    def get_age(self):
+        return self.__age
     
-    def age(self):
-        return self.__stu_age
-    
-    def registration_date(self):
+    def get_register(self):
         return self.__register
               
 
--- StudentProgram.py
+++ StudentProgram.py
@@ -1,2 +1,18 @@
 
 
+import StudentClass as sc
+
+studentid = 1001
+name = 'John'
+dob = '10/11/2001'
+classification = 'junior'
+
+student1 = sc.Student(studentid, name, dob, classification)
+
+student1.calc_age()
+student1.calc_register()
+
+print(f"Student age is: {student1.get_age()}")
+print(f"Student can register between: {student1.get_register()}")
+
+
